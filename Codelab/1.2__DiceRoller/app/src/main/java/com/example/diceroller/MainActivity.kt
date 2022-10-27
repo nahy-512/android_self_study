d54524c61a2184dba014b6d9461960638c8db4a2
nahy-512 <hedghog1004@naver.com>
2022-10-28 05:25:14 +0900

    안드로이드 학습 01.3: Image resources and compatibility

--- Codelab/1.2__DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Codelab/1.2__DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton : Button = findViewById(R.id.roll_btn)
         val countUpButton : Button = findViewById(R.id.countup_btn)
@@ -25,32 +31,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_tv)
 
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
 
     private fun countUp() {
-        val resultText : TextView = findViewById(R.id.result_tv)
 
         Toast.makeText(this, "count up clicked", Toast.LENGTH_SHORT).show()
 
-        // If text is the default "Hello World!" set that text to 1.
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            // Otherwise, increment the number up to 6.
-            // it needs to be converted to a String object before it can be converted to an int.
-            var resultInt = resultText.text.toString().toInt()
-
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
-        }
+//        // If text is the default "Hello World!" set that text to 1.
+//        if (resultText.text == "Hello World!") {
+//            resultText.text = "1"
+//        } else {
+//            // Otherwise, increment the number up to 6.
+//            // it needs to be converted to a String object before it can be converted to an int.
+//            var resultInt = resultText.text.toString().toInt()
+//
+//            if (resultInt < 6) {
+//                resultInt++
+//                resultText.text = resultInt.toString()
+//            }
+//        }
     }
 }

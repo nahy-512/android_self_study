bece65f4874cff75b919776a53498475e8388a53
nahy-512 <hedghog1004@naver.com>
2022-11-01 12:18:25 +0900

    안드로이드 학습 02.2: Add user interactivity

--- Codelab/2.1__AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Codelab/2.1__AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText : EditText
+    lateinit var nicknameTextView : TextView
+    lateinit var doneButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.nickname_edit)
+        nicknameTextView = findViewById(R.id.nickname_text)
+        doneButton = findViewById(R.id.done_button)
+
+        doneButton.setOnClickListener {
+            addNickname(it) // it은 인수로 전달된 뷰인 'done_button'을 나타냄
+        }
+
+        nicknameTextView.setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+        nicknameTextView.text = editText.text
+
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname(view: View) {
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        // Set the focus to the edit text
+        editText.requestFocus()
+
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
